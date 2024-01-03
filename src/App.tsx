0d11f26057e5bc6c1729d85075a726ef7acf0022
Petar <petar.purac@gmail.com>
2024-01-03 19:00:08 +0100

    Install react router, setup basic for routing, create first page-HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.scss';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
+    <>
     <div>
       Real Estate App
     </div>
+      <BrowserRouter>
+      <Routes>
+        <Route  path="/" element={<HomePage/>} />
+      </Routes>
+    </BrowserRouter>
+    </>
   );
 }
 
